8e27e262b8636524ff8e2949061b5bd2921e01f1
francis-collab <f.mutabazi1@alustudent.com>
2025-06-22 08:25:25 -0700

    Updated 0-pascal_triangle.py

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Pascal Triangle Interview Challenge"""
-
+  
 
 def pascal_triangle(n):
-    """returns a list of lists of numbers
+    """Returns a list of lists of numbers
     representing the pascal triangle"""
     if n <= 0:
         return []
@@ -12,9 +12,8 @@ def pascal_triangle(n):
 
     for i in range(1, n):
         row = [1]
-
         for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
         row.append(1)
         triangle.append(row)
 
